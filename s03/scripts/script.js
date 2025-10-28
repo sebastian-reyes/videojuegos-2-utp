883dba26510905772fa96ca180903c20bd624c46
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-10-27 19:42:40 -0500

    s04: agregando logica para eliminar enemigos, con colisiones

--- s03/scripts/script.js
+++ s03/scripts/script.js
@@ -13,12 +13,23 @@ var estadoPrincipal = {
         juego.load.image('fondo', 'assets/img/bg.png');
         juego.load.spritesheet('animacion', 'assets/img/spritesheet1.png', 256, 256);
         juego.load.spritesheet('enemigo', 'assets/img/enemigo1.png', 48, 48);
+        juego.load.image('laser', 'assets/img/laser.png');
     },
 
     create: function () {
         fondoJuego = juego.add.tileSprite(0, 0, 370, 600, 'fondo');
         personaje = juego.add.sprite(80, 380, 'animacion')
         personaje.animations.add('movimiento', [0, 1, 2, 3, 4], 10, true);
+
+        botonDisparo = juego.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        balas = juego.add.group();
+        balas.enableBody = true;
+        balas.physicsBodyType = Phaser.Physics.ARCADE;
+        balas.createMultiple(20, 'laser');
+        balas.setAll('anchor.x', 0.5);
+        balas.setAll('anchor.y', 0.5);
+        balas.setAll('outOfBoundsKill', true);
+        balas.setAll('checkWorldBounds', true);
 
         enemigos = juego.add.group();
         enemigos.enableBody = true;
@@ -56,7 +67,29 @@ var estadoPrincipal = {
         } else {
             personaje.animations.stop();
         }
+
+        var bala;
+        if (botonDisparo.isDown) {
+            if (juego.time.now > tiempoBala) {
+                bala = balas.getFirstDead();
+                if (bala) {
+                    // calcular el centro del personaje en coordenadas de mundo
+                    var centerX = personaje.x + personaje.width * (0.5 - personaje.anchor.x);
+                    var centerY = personaje.y + personaje.height * (0.5 - personaje.anchor.y);
+                    // spawnear la bala en la parte superior central del personaje
+                    bala.reset(centerX, centerY - personaje.height * 0.5);
+                    bala.body.velocity.y = -300;
+                    tiempoBala = juego.time.now + 400;
+                }
+            }
+        }
+        juego.physics.arcade.overlap(balas, enemigos, colision, null, this);
     }
+}
+
+function colision(bala, enemigo) {
+    bala.kill();
+    enemigo.kill();
 }
 
 juego.state.add('principal', estadoPrincipal);
